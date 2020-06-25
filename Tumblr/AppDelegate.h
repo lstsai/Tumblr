9d9ba9df9a805628e1d9eb2dafdffebe543e0546
Lauren Tsai <laurentsai@fb.com>
2020-06-25 08:48:15 -0700

    Initial Commit

---
+++ Tumblr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tumblr
+//
+//  Created by laurentsai on 6/25/20.
+//  Copyright © 2020 laurentsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tumblr/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Tumblr
+//
+//  Created by laurentsai on 6/25/20.
+//  Copyright © 2020 laurentsai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
